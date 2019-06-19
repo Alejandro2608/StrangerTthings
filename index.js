1d7db65f38a5a604e7d9ea2bd5b45b0c9fc11fcb
Alejandro Ortega <alejo.ortegaa@gmail.com>
2019-06-19 06:00:34 -0500

    Stranger Things Actualizado

---
+++ index.js
@@ -0,0 +1,29 @@
+var poderMistico = 0;
+var names = "";
+var artefactos= 0;
+var totalAtefactos = 0;
+var valorArtefactos = 0;
+
+//
+artefactos = prompt("Indique el valor del artefacto de entre 1 y 100");
+totalAtefactos = prompt("Digite la cantidad de artefactos por persona")
+
+for(var i=0; i == names; i++){
+    names = prompt ("Escriba el nombre ")
+
+}
+
+//
+function cantidadArtefactos(
+artefacto, names
+){
+if(artefactos>=1 && artefactos <=3){
+    return "La cantidad de artefactos es: " + artefactos;
+}else{
+    return "Cantidad invalida, por favor vuelva a ingresar el dato";
+}
+
+}
+function totalValorArtefactos(
+    valorArtefactos
+)
