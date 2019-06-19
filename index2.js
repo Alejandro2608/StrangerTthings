8bdbb7eefdc5652b521bad886bd0dc9a78f85b49
Sala 310 <sala310@poliacademico.edu.loc>
2019-06-19 10:12:28 -0500

    Stranger Things Actualizado

---
+++ index2.js
@@ -0,0 +1,28 @@
+var totalPuntos = 1230;
+var amigos = ["Mike", "Lucas", "Dustin", "Eleven", "Max"];
+var artefactos = 3;
+var puntosArtefactos = [0,0,0,0,0];
+
+for(var i=0; i<amigos.length;i++){
+   
+    
+    
+    for(var j=0; j<artefactos;j++){
+        do{
+
+        
+        var artefacto=prompt("Digite los puntos del artefacto "+(j+1)+
+        " recolectado por "+amigos[i]);
+        artefacto=parseInt(artefacto);
+    }while(artefacto<1 || artefacto >100)
+        puntosArtefactos[i] += artefacto;
+
+        alert("El total de puntos va en " + puntosArtefactos[i]);
+    }
+
+    
+}
+for(var k=0;k<puntosArtefactos.length; k++){
+    document.write("<h3>Los puntos que recogio " +amigos[k]+ 
+    " fueron en total" +puntosArtefactos);
+}
